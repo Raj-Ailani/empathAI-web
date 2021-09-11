82c8f579cd946e9be093d1e8fd54bbfd35b4920d
Raj-Ailani <ailaniraj1@gmail.com>
2021-09-11 23:24:43 +0530

    Added add prduct feature

--- src/screens/AddProduct.js
+++ src/screens/AddProduct.js
@@ -1,11 +1,69 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-
+import React, { useEffect, useState } from 'react'
+import { Button, FormControl, FormLabel } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { Container, Form, FormGroup,Row,Col } from 'reactstrap'
+import { login } from '../actions/userActions'
+import { Link} from 'react-router-dom';
+// name:{
+//     type:String,
+//     required:true,
+// },
+// description:{
+//     type:String,
+//     required:true,
+// },
+// image:{
+//   type:String,
+//   required:true,
+// },
+// avgPrice:{
+//   type:Number,
+//   required:true,
+// },
+//    details:[{
+//    title:String,
+//    desp:String
+//  }]
 const AddProduct = () => {
     return (
         <>
-            <Container fluid >
+            <Container id='add-product-cont' fluid >
                 <h3>Add Product</h3>
+               <Container fluid>
+                <Form  id='form'> 
+                <Row>
+                    <Col>
+                    <FormGroup controlId='email' >
+                 <FormLabel>Name: </FormLabel>
+                 <FormControl type='name' placeholder='Enter Name' 
+                ></FormControl>
+             </FormGroup>
+                    </Col>
+                    <Col>
+                    <FormGroup controlId='email' >
+                 <FormLabel>Average Price: </FormLabel>
+                 <FormControl type='number' placeholder='Enter Price' 
+                ></FormControl>
+             </FormGroup>
+                    </Col>
+                </Row>
+
+                <Row>
+                <FormGroup controlId='review' >
+                <FormLabel>Product Description: </FormLabel>
+                <textarea id='big-box' type='description' placeholder='Enter Description'></textarea>
+                </FormGroup>    
+                </Row>
+              
+
+
+             <Button type='submit' varient='primary'>
+                Add
+             </Button>
+           
+
+         </Form>
+         </Container>
             </Container>
         </>
     )
