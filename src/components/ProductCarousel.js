ab02ab4342ded5f66bae8f5ee4b7c7dba6bc1d08
Raj-Ailani <ailaniraj1@gmail.com>
2021-09-24 16:31:01 +0530

    Changed Landing Page

--- src/components/ProductCarousel.js
+++ src/components/ProductCarousel.js
@@ -3,6 +3,7 @@ import { Carousel } from 'react-responsive-carousel';
 import React from 'react'
 import { Link} from 'react-router-dom';
 import { useHistory } from "react-router-dom";
+import { Button, Col, Container, Row } from "reactstrap";
 
 const ProductCarousel = () => {
     let history = useHistory();
@@ -12,20 +13,27 @@ const ProductCarousel = () => {
     }
     return (
         <div>
-            <Carousel showThumbs={false}
-            showStatus={false}
-            showArrows={false}
-            showIndicators={false}
-            onClickItem	={()=>{test()}}
-            infiniteLoop>
-                <div>   
-                    
-                <img id='test' src='/assets/1.jpg' alt='landing-img'></img>
+            <Container id='caro' fluid>
+                <Row>
+                    <Col lg={5} md={12} sm={12} xs={12}>
                    
-                </div>
-      
-        
-            </Carousel>
+                   <h3 id='landing-title'><span id='red'>AI</span>-Driven Sentiment Analysis</h3>
+                   <h5 id='landing-title2'>Internet monitoring tool combined with advanced sentiment analysis working together
+                    to protect your brand, analyze your audience opinions, and connect you with potential customers</h5>
+                    <Row>
+                    <Link id='about-link' to='/about'> <h5 id='landing-title3'><span id='red_hover'>Know More About Us  <i class="fas fa-long-arrow-alt-right"></i>
+                    </span>
+                    </h5>
+                    </Link>
+                    </Row>
+                    </Col>
+                    <Col className='d-none d-sm-none d-md-none d-lg-block' md={7} sm={7} xs={7}>
+                    <img id='test' src='/assets/bg-test.png' alt='bg'></img>
+                    </Col>
+                </Row>
+  
+            </Container>
+         
         </div>
     )
 }
